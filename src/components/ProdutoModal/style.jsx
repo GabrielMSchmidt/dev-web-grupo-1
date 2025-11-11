157a01a90b2e9fab107eb3f32f30eb2056866f88
GabrielMSchmidt <gabrielmarochischmidt@gmail.com>
2025-11-11 17:14:35 -0300

    Add: Select Categorias

--- src/components/ProdutoModal/style.jsx
+++ src/components/ProdutoModal/style.jsx
@@ -99,7 +99,22 @@ export const Input = styled.input`
     &:focus {
         border-color: #007bff;
         outline: none;
-        box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+    }
+`;
+
+export const Select = styled.select`
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ced4da;
+    border-radius: 4px;
+    font-size: 16px;
+    box-sizing: border-box;
+    background-color: white;
+    cursor: pointer;
+    
+    &:focus {
+        border-color: #007bff;
+        outline: none;
     }
 `;
 
