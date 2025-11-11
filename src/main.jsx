0afbc1d09f2fb4e2502d5939e3cec57b8942c0f1
FERNANDO H  M CAMPOS <braaboozx@gmail.com>
2025-11-10 22:04:55 -0300

    Correção do erro de scroll no eixo x

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { Routers } from './routers/Routers.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ModalChatbot } from './components/ModalChatbot/index.jsx'
+import {
+  Container
+} from './mainStyle.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routers />
-      <ModalChatbot />
-    </BrowserRouter>
+    <Container>
+      <BrowserRouter>
+        <Routers />
+        <ModalChatbot />
+      </BrowserRouter>
+    </Container>
   </StrictMode>,
 )
